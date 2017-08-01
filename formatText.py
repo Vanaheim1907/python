804948e81122eafc189770fd9c7e19b80880ef0b
Vanaheim <brosteaualexandre@icloud.com>
2017-08-01 12:38:31 +0200

    complete error

---
+++ formatText.py
@@ -0,0 +1,4 @@
+def create_message(character,quote):
+    "{character} a dit {}:".format(character,quote)
+    
+create_message("Yoda","Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.")
