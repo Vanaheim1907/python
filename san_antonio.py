4c497275dd5507ea952bf6ee3c639003fe5d885a
Vanaheim <brosteaualexandre@icloud.com>
2017-08-01 21:02:03 +0200

    add random citation

--- san_antonio.py
+++ san_antonio.py
@@ -1,25 +1,32 @@
-#Show random quote
+import random
 
-#If user_answer is 'B' :
-# - leave the program
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
 
-# Else :
-# - show another quote
-
-#Function
-def show_random_quote(my_list):
-	#show another quote
-	# TODO: get a random number
-	item = my_list[0] # get a quote from a list. For the moment, just get the first one.
-	print(item) # show the quote in the interpreter
-	return "program is over" # returned value
-	
-#Variables
-quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."] 
 characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
 
-user_answer = "A"
+
+def get_random_item(my_list):
+    rand = random.randint(0,10)# get a random number
+    item = my_list[rand] # get a quote from a list
+    return item # return the item
+
+
+def capitalize(words):
+    for word in words:
+        return word.capitalize()
+
+
+def message(character, quote):
+    n_character = capitalize(character)
+    n_quote = capitalize(quote)
+    return "{} a dit : {}".format(n_character, n_quote)
+
+
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
+
 
 while user_answer != "B":
-	print(show_random_quote(quotes))
-	user_answer = "B"
+    print(message(get_random_item(characters), get_random_item(quotes)))
+    user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
+
+
